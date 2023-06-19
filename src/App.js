b41a10dca6a8ad2b83964b2563093a72778defe6
Gautham Kamath Kinnimulky <gauthamkamathk24@gmail.com>
2023-06-19 03:41:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { LandingPage } from "./components/LandingPage";
 import { NewHabitDialog } from "./components/NewHabitDialog";
 import "./styles.css";
 import { AboutHabit } from "./components/AboutHabit";
+import { EditHabit } from "./components/EditHabit";
+import { ArchivedHabits } from "./components/ArchivedHabits";
 
 export default function App() {
   return (
     <div className="App">
-      {/* <LandingPage /> */}
-
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/create" element={<NewHabitDialog />}></Route>
-        <Route path="/about/:imageId" element={<AboutHabit />}></Route>
+        <Route path="/edit/:habitId" element={<EditHabit />}></Route>
+        <Route path="/about/:habitId" element={<AboutHabit />}></Route>
+        <Route path="/archived" element={<ArchivedHabits />}></Route>
       </Routes>
     </div>
   );
